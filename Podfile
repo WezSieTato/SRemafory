2dc0c0562a49ac8c7f589ecce5860cab97765611
WezSieTato <le0n.uo@gmail.com>
2015-01-14 15:27:35 +0100

    Create workspace

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+inhibit_all_warnings!
+
+target 'SRemafory' do
+    pod 'ProtocolBuffers', '~> 1.9'
+    pod 'libzmq', '~> 4.0'
+end
+
---
+++ SRemafory/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SRemafory
+//
+//  Created by Marcin Stepnowski on 14/01/15.
+//  Copyright (c) 2015 siema. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
